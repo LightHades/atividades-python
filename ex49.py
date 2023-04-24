d748a0bc5e5e0331e0b03ba171c664cc856e933e
camilotrouy <lhades1803@gmail.com>
2023-04-24 17:51:28 -0400

    Atividades adicionadas

---
+++ ex49.py
@@ -0,0 +1,4 @@
+dolar = float(input('Cotacao do dolar: R$ '))
+valor = float(input('Valor e dolares: $'))
+dolar_to_real = valor * dolar
+print(f'O valor em real é R$ {dolar_to_real:.2f}')
---
+++ ex50.py
@@ -0,0 +1,6 @@
+valor = int(input('Valor: '))
+
+antecessor = valor - 1
+sucessor = valor + 2
+
+print(f'Ancetessor: {antecessor}\nSucessor: {sucessor}')
